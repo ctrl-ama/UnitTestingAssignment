dd9922a550d9f98dfcfe77057b085ca60feeaa15
Amayindi <amayindi.yerima@binghamuni.edu.ng>
2023-10-13 08:57:57 -0700

    Ama's commit

--- src/TestFibonacci.java
+++ src/TestFibonacci.java
@@ -8,16 +8,16 @@ public class TestFibonacci {
         int[] expected = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
         int[] fibonacci = new int[n];
 
-        // Initialize the first two Fibonacci numbers
+        // Initializing the first two Fibonacci numbers
         fibonacci[0] = 0;
         fibonacci[1] = 1;
 
-        // Calculate the remaining Fibonacci numbers
+        //  To calculate the remaining Fibonacci numbers
         for (int i = 2; i < n; i++) {
             fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
         }
 
-        // Use assertArrayEquals to compare the expected Fibonacci sequence with the generated one
+        // Using assertArrayEquals to compare the expected Fibonacci sequence with the generated one
         assertArrayEquals(expected, fibonacci);
     }
 }
