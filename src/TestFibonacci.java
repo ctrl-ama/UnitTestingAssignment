6fb05bb82961e1dba8a83eadef568abd505800e2
Amayindi <amayindi.yerima@binghamuni.edu.ng>
2023-10-12 17:17:11 -0700

    Ama's commit

---
+++ src/TestFibonacci.java
@@ -0,0 +1,11 @@
+public class TestFibonacci {
+
+    private Fibonacci fibonacci;
+
+    @Before
+    public void setUp() {
+        // Initialize any resources or objects needed before each test case.
+        fibonacci = new Fibonacci();
+
+    }
+}
