274842ed8fa02c873e26dd868fd614eefc2f5659
Tyshe-81 <adeniyioluwatishe@gmail.com>
2023-10-13 17:33:43 -0700

    commit done

--- src/TestFibonacci.java
+++ src/TestFibonacci.java
@@ -5,7 +5,8 @@ public class TestFibonacci {
     @Test
     public void testFirstTenFibonacciNumbers() {
         int n = 10; // The number of Fibonacci numbers to generate
-        int[] expected = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
+        int[] expected = { 0, 1, 1, 2, 3, 5, 8, 13, 21,34};
+
         int[] fibonacci = new int[n];
 
         // Initializing the first two Fibonacci numbers
@@ -13,7 +14,7 @@ public class TestFibonacci {
         fibonacci[1] = 1;
 
         //  To calculate the remaining Fibonacci numbers
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <n; i++) {
             fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
         }
 
